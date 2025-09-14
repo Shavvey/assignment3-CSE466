8b788aa781b10bd64c55825f10ddce3e2b5bdabc
Cole Johnson <coledjohn@gmail.com>
2025-09-14 17:43:40 -0600

    Implement basic kNNs w/ simple majority voting

--- distance.py
+++ distance.py
@@ -1,4 +1,3 @@
-import numpy.typing as npt
 import math as m
 from point import Point
 
@@ -12,7 +11,12 @@ class Distance:
         for i in range(x.dims):
             sum += dist(x.coords[i], y.coords[i])
         return sum
-    
+
     @staticmethod
     def Manhattan(x: Point, y: Point) -> float:
         assert x.dims == y.dims
+        dist = lambda x, y: abs(x - y)
+        sum = 0
+        for i in range(x.dims):
+            sum += dist(x.coords[i], y.coords[i])
+        return sum
--- knns.py
+++ knns.py
@@ -5,6 +5,8 @@ import numpy.typing as npt
 from point import Point
 from distance import Distance
 
+type PointDistance = tuple[Point, float]
+
 class VotingType(Enum):
     MAJORITY = 1
     DISTANCE = 2
@@ -12,6 +14,8 @@ class VotingType(Enum):
 def main():
     points = Point.points_from_iris_csv("data/iris_rnd_train.csv")
     preds = KNearestNeigbors(points, Distance.Euclidean, VotingType.MAJORITY, 5)
+    for i, pred in enumerate(preds):
+        print(f"{i}: {pred}")
 
 
 def KNearestNeigbors(
@@ -28,13 +32,16 @@ def KNearestNeigbors(
             ldata = np.append(ldata, [point])
     # create predictions for each unlabeled piece of data
     for pred in preds:
-        distances = (pred, ldata, dist, k)
+        distances = make_distances(pred, ldata, dist, k)
+        label = make_prediction(vtype, distances)
+        pred.label = label
     return preds
 
 
 def make_distances(
     pred: Point, ldata: npt.NDArray, dist: FunctionType, k: int
-) -> list[tuple[Point, float]]:
+) -> list[PointDistance]:
+    "Given a prediction point, make a list of minimum distances to `k` points the data"
     # find all distances to labeled points
     pdist = []
     for ld in ldata:
@@ -42,6 +49,23 @@ def make_distances(
         pdist.append((ld, distance))
     pdist.sort(key=lambda x: x[1])
     return pdist[0:k]
+
+def make_prediction(vtype: VotingType, distances: list[PointDistance]) -> str:
+    # map each label to voting power
+    match vtype:
+        case VotingType.MAJORITY:
+            vote_map: dict[str, int] = {}
+            for pdist in distances:
+                label = pdist[0].label
+                if label != None:
+                    if vote_map.get(label) == None:
+                        vote_map[label] = 1
+                    else:
+                        vote_map[label] += 1
+            pred = sorted(vote_map, key=lambda x: x[0])[0]
+            return pred
+        case _:
+            raise ValueError("[ERROR]: Voting type not implemented!")
 
 if __name__ == "__main__":
     main()
