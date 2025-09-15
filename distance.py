c13934a35149d44aafb41e3b3d5b71fcae97c7db
Cole Johnson <coledjohn@gmail.com>
2025-09-15 09:35:13 -0600

    Migrate unittest to seperate file

--- distance.py
+++ distance.py
@@ -1,6 +1,5 @@
 import math as m
 from point import Point
-import unittest
 
 
 class Distance:
@@ -21,15 +20,3 @@ class Distance:
         for i in range(x.dims):
             sum += dist(x.coords[i], y.coords[i])
         return sum
-
-
-class TestDistances(unittest.TestCase):
-    def test_euclidean(self):
-        x = Point((3, 5))
-        y = Point((6, 8))
-        self.assertEqual(Distance.euclidean(x, y), m.sqrt(18))
-
-    def test_manhattan(self):
-        x = Point((3, 5))
-        y = Point((6, 8))
-        self.assertEqual(Distance.manhattan(x, y), 6)
---
+++ distance_test.py
@@ -0,0 +1,15 @@
+import unittest
+from distance import Distance
+from point import Point
+import math as m
+
+class TestDistances(unittest.TestCase):
+    def test_euclidean(self):
+        x = Point((3, 5))
+        y = Point((6, 8))
+        self.assertEqual(Distance.euclidean(x, y), m.sqrt(18))
+
+    def test_manhattan(self):
+        x = Point((3, 5))
+        y = Point((6, 8))
+        self.assertEqual(Distance.manhattan(x, y), 6)
