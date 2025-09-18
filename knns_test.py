df9dc4f0c16b49323a23138aad4635f98858f1ed
Cole Johnson <coledjohn@gmail.com>
2025-09-18 17:10:00 -0600

    Create new testing strategy to avoid ties
    - Make test such that we have k-1 labels for each k we are testing, this
    shoudl avoid the possbility of ties during majority voting

--- knns_test.py
+++ knns_test.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import random
 
+
 class TestKNearestNeighbors(unittest.TestCase):
     # first, test w/ k = 1
     def test_knn_1(self):
@@ -23,25 +24,28 @@ class TestKNearestNeighbors(unittest.TestCase):
         self.assertEqual(labels, ["3", "2", "1"])
 
     def test_knn_2(self):
-        labels = ['A', 'B', 'C']
-        skPoints = [(random.randint(1, 10), random.randint(1, 10)) for _ in range(30)]
-        skLabels = [random.choice(labels) for _ in range(30)]
-        points = [Point(point, label) for point, label in zip(skPoints, skLabels)]
+        ks = 10
+        for _ in range(ks):
+            if ks % 2 != 0:
+                labels = [str(k) for k in range(ks-1)]
+                skPoints = [(random.randint(1, 10), random.randint(1, 10)) for _ in range(30)]
+                skLabels = [random.choice(labels) for _ in range(30)]
+                points = [Point(point, label) for point, label in zip(skPoints, skLabels)]
 
-        pred_points = [Point((1, 1)), Point((9, 9)), Point((4, 4))]
-        points.extend(pred_points)
-        points = np.array(points, dtype=Point)
-        skPred = [(1, 1), (9, 9), (4, 4)]
-        preds = KNearestNeigbors(points, Distance.euclidean, VotingType.MAJORITY, 3)
-        predLabels = []
+                pred_points = [Point((1, 1)), Point((9, 9)), Point((4, 4))]
+                points.extend(pred_points)
+                points = np.array(points, dtype=Point)
+                skPred = [(1, 1), (9, 9), (4, 4)]
+                preds = KNearestNeigbors(points, Distance.euclidean, VotingType.MAJORITY, ks)
+                predLabels = []
 
-        for pred in preds:
-            predLabels.append(pred.label)
-        knn = KNeighborsClassifier(n_neighbors=3, weights="uniform", metric="euclidean")
-        knn.fit(skPoints, skLabels)
-        skPredLabels = knn.predict(skPred)
-        np.testing.assert_array_equal(skPredLabels, predLabels)
-        
+                for pred in preds:
+                    predLabels.append(pred.label)
+                knn = KNeighborsClassifier(n_neighbors=ks, weights="uniform", metric="euclidean")
+                knn.fit(skPoints, skLabels)
+                skPredLabels = knn.predict(skPred)
+                np.testing.assert_array_equal(skPredLabels, predLabels)
+
 
 if __name__ == "__main__":
     unittest.main()
